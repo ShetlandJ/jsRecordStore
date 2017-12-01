986ea0ee3caada156d2bad5092e003734b4fd5e2
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-01 15:54:26 +0000

    Record Collector can add items to inventory

--- specs/record_collector_spec.js
+++ specs/record_collector_spec.js
@@ -8,11 +8,19 @@ describe( "Record Collector", function(){
   var recordCollector;
 
   beforeEach(function(){
+    record1 = new Record("Les Savy Fav", "Go Forth", "Rock", 8);
+
     recordCollector = new RecordCollector("James", 100);
   });
 
   it("should have a name", function(){
     assert.strictEqual(recordCollector.name, "James");
   });
+
+  it("should be able to add items to their inventory", function(){
+    recordCollector.add(record1)
+    assert.strictEqual(recordCollector.collection.length, 1);
+  });
+
 
 });
