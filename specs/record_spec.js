13ad0f4aebdabd06c558bfe4ac2cc53acaf646fb
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-01 14:10:57 +0000

    set up test file for record with four first tests

---
+++ specs/record_spec.js
@@ -0,0 +1,18 @@
+var assert = require("assert")
+var Record = require("../record")
+
+describe( "Record", function(){
+
+  var record;
+
+  beforeEach(function(){
+    record = new Record();
+  });
+
+  it("should have an artist");
+  it("should have a title");
+  it("should have a genre");
+  it("should have a price");
+
+
+});
